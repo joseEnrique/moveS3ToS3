e33f880f54c3b9073ef19d79b594f2d177ca1cc6
Quique <quique@podgroup.com>
2021-03-14 17:40:25 +0100

    added update per process

--- test_for.py
+++ test_for.py
@@ -6,12 +6,23 @@ session = generate_session()
 
 
 def get_not_processed_key():
-    while session.query(Pictures).filter(Pictures.new_route == None).count() > 0:
-        yield session.query(Pictures).filter(Pictures.new_route == None).first()
-    #for not_new_route_element in session.query(Pictures).filter(Pictures.new_route == None):
+    while session.query(Pictures).filter(Pictures.new_route is None).count() > 0:
+        # ran_object = session.query(Pictures).filter(Pictures.new_route == None).order_by(func.rand()).first()
+        ran_object = session.query(Pictures).filter(Pictures.new_route is None).order_by(func.rand()).first()
+        yield ran_object
+    # for not_new_route_element in session.query(Pictures).filter(Pictures.new_route == None):
     #    yield not_new_route_element.old_route
 
 
-for i in get_not_processed_key():
-    print (i)
+def update_element(pic):
+    try:
+        pic.new_route = 'updated'
+        session.add(pic)
+        session.commit()
+    except:
+        session.rollback()
 
+
+for i in get_not_processed_key():
+    update_element(i)
+    print(session.query(Pictures).filter(Pictures.new_route is None).count())
